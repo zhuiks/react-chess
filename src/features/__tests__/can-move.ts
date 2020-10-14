ab12cdb6b986e47105beaba5bf64af0b6651e538
zhuiks <zhuke@ukr.net>
2020-10-14 12:54:37 -0500

    refactor test

--- src/features/__tests__/can-move.ts
+++ src/features/__tests__/can-move.ts
@@ -1,24 +1,41 @@
 import canMove from '../can-move'
-import { BoardState } from '../types'
+import { BoardState, BPType, Move, MoveStart, SquareStateType } from '../types'
 
+const getOnePieceBoardOptions = (piece: SquareStateType, from: BPType) => {
+  const set: BoardState = {
+    [from]: piece
+  }
+  return canMove(piece, from, set)
+}
 describe('Features/Move Options', () => {
   describe('Pawn', ()=>{
+    const piece: SquareStateType = '♙'
     it('can move two squares up', () => {
-      const set: BoardState = {
-        e2: '♙'
-      }
-      const options = canMove('♙', 'e2', set)
+      const options = getOnePieceBoardOptions(piece, 'e2')
       expect(options).toContain('e3')
       expect(options).toContain('e4')
     })
     it('can move only one square up', () => {
-      const set: BoardState = {
-        a5: '♙'
-      }
-      const options = canMove('♙', 'a5', set)
+      const options = getOnePieceBoardOptions(piece, 'a5')
       expect(options).toContain('a6')
       expect(options).not.toContain('a7')
     })
-  
+  })
+  describe('Rook', ()=>{
+    const piece: SquareStateType = '♜'
+    it('can move verticaly', ()=>{
+      const set: BoardState = {
+        f5: '♜'
+      }
+      const options = getOnePieceBoardOptions(piece, 'f5')
+      expect(options).toContain('f6')
+      expect(options).toContain('f7')
+      expect(options).toContain('f8')
+      expect(options).toContain('f1')
+      expect(options).toContain('f2')
+      expect(options).toContain('f3')
+      expect(options).toContain('f4')
+      expect(options).toHaveLength(7)
+    })
   })
 })
